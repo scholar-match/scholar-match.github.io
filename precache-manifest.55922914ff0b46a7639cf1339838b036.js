6b767dcfa458452c834169b2b35e80f62694d114
Simeon McGraw <simeon@smcgraw.com>
2019-04-01 19:47:08 -0400

    Initial commit

---
+++ precache-manifest.55922914ff0b46a7639cf1339838b036.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f634be3ab5a01e262dd6",
+    "url": "/static/css/main.781f37b2.chunk.css"
+  },
+  {
+    "revision": "f634be3ab5a01e262dd6",
+    "url": "/static/js/main.4d0cfb2c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "75cddb2b241f42e290bb",
+    "url": "/static/js/2.553b7657.chunk.js"
+  },
+  {
+    "revision": "6ecb3854283d7cd91fbcf3d27974c012",
+    "url": "/index.html"
+  }
+];
